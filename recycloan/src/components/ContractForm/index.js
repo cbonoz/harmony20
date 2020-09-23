d8a8b493151f03cbcb4493bb49af36e4f9d106bb
cbonoz <chrisdistrict@gmail.com>
2020-09-23 11:12:26 -0400

    update layout

--- recycloan/src/components/ContractForm/index.js
+++ recycloan/src/components/ContractForm/index.js
@@ -34,32 +34,37 @@ const ContractForm = () => (
         /* and other goodies */
       }) => (
         <form onSubmit={handleSubmit}>
-          <div class="field has-addons has-addons-centered">
-            <label className="title is-4">
+          <div className="field has-addons has-addons-centered">
+            <div className="title is-4 clear">
               Enter addresses separated by commas:
-            </label>
-            <input class="input" type="text" placeholder="Addresses" />
+            </div>
+            <br />
+            <textarea
+              className="input clear"
+              type="text"
+              placeholder="Addresses"
+            />
             {/* {TODO} */}
           </div>
-          <div class="field has-addons has-addons-centered">
-            <p class="control">
+          <div className="field has-addons has-addons-centered">
+            <p className="control">
               <img src="https://assets.coingecko.com/coins/images/4344/small/Y88JAze.png?1565065793" />
             </p>
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Amount of Harmony (ONE) to lend"
             />
-            <p class="control">
-              <a class="button is-primary">Start Loan</a>
+            <p className="control">
+              <a className="button is-primary">Start Loan</a>
             </p>
           </div>
           <div
-            class="field is-grouped is-grouped-right"
+            className="field is-grouped is-grouped-right"
             disabled={isSubmitting}
           >
-            <p class="control">
-              <a class="button is-light">Clear</a>
+            <p className="control">
+              <a className="button is-light">Clear</a>
             </p>
           </div>
         </form>
--- recycloan/src/components/Home/index.js
+++ recycloan/src/components/Home/index.js
@@ -1,26 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ContractForm from "../ContractForm";
+import logo from "../../assets/recycloan_white.png";
 
 import "./Home.css";
 
 export default function Home() {
+  const [result, setResult] = useState(null);
+
   const body = () => (
     <div className="home-section container">
-      <div className="home-title">Recycloan</div>
-      <div className="home-title">A smart contract for automatic loans</div>
-      <p className="home-subtitle subtitle is-5">
-        Recycloan creates a smart contract that represents a series of loans
-        that go out automatically the moment they are paid back.Login and enter
-        a list of addresses as well as the initial payment and we'll
-        automatically send the funds in order to each address.
-      </p>
+      <div className="home-section-header white">
+        {/* <div className="home-title">Recycloan</div> */}
+        <div className="centered">
+          <img src={logo} className="home-section-logo" />
+        </div>
+        <div className="home-title">Loans without borders</div>
+        <p className="home-subtitle subtitle is-5 white">
+          Recycloan creates a smart contract that represents a series of loans
+          that go out automatically the moment they are paid back. Login with
+          your{" "}
+          <a href="https://www.harmony.one/" target="_blank">
+            harmony
+          </a>{" "}
+          account to enter a list of addresses with an initial payment and we'll
+          automatically lend the funds in order to each address.
+        </p>
+      </div>
       <div class="columns container home-container">
         <div class="column">
           <h1 class="title is-2">Who do you want to loan to?</h1>
           <ContractForm />
         </div>
         <div class="column">
-          <h1 class="title is-2">Result:</h1>
+          {result && <h1 class="title is-2">View your result:</h1>}
         </div>
       </div>
     </div>
