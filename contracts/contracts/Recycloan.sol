0aa4984e9a18974128878b671b0069ee3834091c
Chris Buonocore <chrisdistrict@gmail.com>
2020-09-26 11:28:56 -0400

    amount

--- contracts/contracts/Recycloan.sol
+++ contracts/contracts/Recycloan.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Recycloan {
     address public manager;
     address payable[] public borrowers;
+    uint public amount;
     
     constructor() public {
         manager = msg.sender;
@@ -14,6 +15,16 @@ contract Recycloan {
     
     function getBorrowers() public view returns(address payable[] memory) {
         return borrowers;
+    }
+
+    /**
+     * @dev The player enters into the current lottery session by
+     *      paying at least 0.1 token.
+     */
+    function startLoan(address[] _borrowers, uint _amount) {
+        require(msg.value > .1 ether, INSUFFICIENT_FUND_MESSAGE);
+        borrowers = _borrowers;
+        amount = _amount;
     }
     
     function lendToNextBorrower() public restricted {
